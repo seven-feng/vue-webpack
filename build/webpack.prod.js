57fa8279d326b47a2cdb193c41027fe9473603ed
seven-feng <zfwork@sina.com>
2020-04-21 10:47:17 +0800

    增加 js 压缩

--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -2,6 +2,7 @@ const utils = require('./utils');
 const config = require('./config');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin'); // 实现css分离
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin'); // 这个插件作用是对单独抽离出来的css文件进行压缩
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin') // js 压缩
 const merge = require('webpack-merge');
 const commonConfig = require('./webpack.common.js');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
@@ -31,8 +32,8 @@ const prodConfig = {
           {
             loader: "css-loader", // 解析 @import and url()
             options:{
-                importLoaders:2 ,// 如果sass文件里还引入了另外一个sass文件，另一个文件还会从postcss-loader向上解析。如果不加，就直接从css-loader开始解析。
-                modules: true // 开启css的模块打包。css样式不会和其他模块发生耦合和冲突
+              importLoaders: 2 , // 如果sass文件里还引入了另外一个sass文件，另一个文件还会从postcss-loader向上解析。如果不加，就直接从css-loader开始解析。
+              modules: true // 开启css的模块打包。css样式不会和其他模块发生耦合和冲突
             }
           }, 
           'postcss-loader', // 为 css 样式属性加不同浏览器的前缀
@@ -42,6 +43,7 @@ const prodConfig = {
     ]
   },
   plugins: [
+    new UglifyJsPlugin(),
     new MiniCssExtractPlugin({
       // Options similar to the same options in webpackOptions.output
       // both options are optional
